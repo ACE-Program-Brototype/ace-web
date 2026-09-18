8bc23b573eebc9bdd03c436a9d6f66a9ac67bba5
Venkitesh N S <software.venkitesh.in@gmail.com>
2026-09-18 16:42:48 +0530

    Update launchConfig.js

--- src/constants/launchConfig.js
+++ src/constants/launchConfig.js
@@ -4,4 +4,4 @@
  * Set `IS_LAUNCH_ENABLED` to `true` to enable the inauguration launch intro video.
  * Set to `false` to disable the launch video completely and load the site directly.
  */
-export const IS_LAUNCH_ENABLED = true;
+export const IS_LAUNCH_ENABLED = false;
